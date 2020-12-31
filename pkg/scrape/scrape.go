612e81b8c1f64799e1149d8880d5f873ea48c156
Max Neverov <neverov.max@gmail.com>
2020-12-31 16:23:25 +0100

    Added solution for scraper (http client)

--- pkg/scrape/scrape.go
+++ pkg/scrape/scrape.go
@@ -1,7 +1,13 @@
 package scrape
 
 import (
+	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"time"
+
+	"github.com/pkg/errors"
 
 	"github.com/mneverov/webapp101/pkg/metric"
 )
@@ -35,11 +41,42 @@ func NewHTTPScraper(client httpClient, url, name string) *HTTPScraper {
 // Scrape retrieves ranks and returns the ranks or an error not
 // longer than the configured timeout.
 func (c *HTTPScraper) Scrape() (metric.Metric, error) {
-	m := metric.Metric{}
 	// 1. Create a new http request with the scraper url
-	// 2. Use the scraper client to do the request
-	// 3. Calculate the result body size
-	// 4. measure the time and assemble the Metric
+	req, err := http.NewRequest(http.MethodGet, c.url, nil)
+	if err != nil {
+		return metric.Metric{},
+			errors.Wrapf(err, "failed to create request for %s", c.name)
+	}
 
-	return m, nil
+	start := time.Now()
+	// 2. Use the scraper client to do the request
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return metric.Metric{}, errors.Wrapf(err, "request failed for %s", c.name)
+	}
+
+	defer func() {
+		if err = resp.Body.Close(); err != nil {
+			log.Printf("failed to close response, %s", err)
+		}
+	}()
+
+	// 3. Calculate the result body size
+	bytes, err := io.Copy(ioutil.Discard, resp.Body)
+	if err != nil {
+		return metric.Metric{},
+			errors.Wrapf(err, "failed to read response for %s", c.name)
+	}
+
+	// 4. Measure the time and assemble the Metric
+	responseTime := int(time.Since(start).Milliseconds())
+	m := metric.Metric{
+		Name:              c.name,
+		StatusCode:        resp.StatusCode,
+		ResponseSizeBytes: bytes,
+		ResponseTimeMs:    responseTime,
+		CreatedAt:         time.Now(),
+	}
+
+	return m, err
 }
